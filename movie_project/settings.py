21c56ce2aed67eaeb063274e194e071d7028fab7
minhajahmdtk <mnhjahammed@gmail.com>
2026-02-12 14:17:45 +0530

    Final commit

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1v*@7(9zms$eej@h^b5m&shbo8(5j)o18t3^vzv13u*#zx+ry*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
